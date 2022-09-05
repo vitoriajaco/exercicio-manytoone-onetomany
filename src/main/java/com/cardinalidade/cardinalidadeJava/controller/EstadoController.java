d2ee89e0bb66c2206fec94d80f2e4d7db7baf44e
vitoriajaco <vitoriajaco@gmail.com>
2022-09-04 23:10:28 -0300

    Adicionar controller estado

--- src/main/java/com/cardinalidade/cardinalidadeJava/controller/EstadoController.java
+++ src/main/java/com/cardinalidade/cardinalidadeJava/controller/EstadoController.java
@@ -1,4 +1,36 @@
 package com.cardinalidade.cardinalidadeJava.controller;
 
+import com.cardinalidade.cardinalidadeJava.model.EstadoModel;
+import com.cardinalidade.cardinalidadeJava.service.EstadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class EstadoController {
+
+    @Autowired
+    private EstadoService estadoService;
+
+    @GetMapping(path = "/estados")
+    public List<EstadoModel> buscarTodosEstados(){
+        return estadoService.buscarTodos();
+    }
+
+    @GetMapping(path = "/estados/{codigo}")
+    public Optional<EstadoModel> buscarporId(@PathVariable Long codigo){
+      return  estadoService.buscarId(codigo);
+    }
+
+    @PutMapping (path = "/estados")
+    public EstadoModel alterarEstado (@RequestBody EstadoModel estadoModel){
+        return estadoService.alterar(estadoModel);
+    }
+
+    @DeleteMapping(path = "/estados/{codigo}")
+    public void deletarEstado(@PathVariable Long codigo){
+        estadoService.deletar(codigo);
+    }
 }
